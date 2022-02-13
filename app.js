efe81ff156fb1f7a3eac898f55cfd960c586726b
Diego H <d13g01hl@gmail.com>
2022-02-13 15:54:25 -0500

    Added About

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ let Error404 = {
     }
 };
 
+let About = {
+    render : async () => {
+        let view = /*html*/
+        `
+            <section class="section">
+                <h1>About</h1>
+            </section>
+        `;
+        return view;
+    }
+};
+
 let Register = {
     render : async () => {
         return /*html*/ `
