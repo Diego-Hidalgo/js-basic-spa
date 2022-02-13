9d4ea58dcc5b2534f698dc85166515261f6000b0
Diego H <d13g01hl@gmail.com>
2022-02-13 15:31:26 -0500

    Added Utils

---
+++ app.js
@@ -0,0 +1,24 @@
+const Utils = {
+    parseRequestURL : () => {
+        let url = location.hash.slice(1).toLowerCase() || '/';
+        let r = url.split("/")
+        let request = {
+            resource    : null,
+            id          : null,
+            verb        : null
+        }
+        request.resource    = r[1]
+        request.id          = r[2]
+        request.verb        = r[3]
+
+        return request
+    }
+};
+
+const routes = {
+    '/'             : Home
+    , '/about'      : About
+    , '/p/:id'      : PostShow
+    , '/register'   : Register
+    , '/login'      : LogIn
+};
